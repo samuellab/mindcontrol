d02e2756fbe87e19bcd46d0ae92686c12ad75ce5
Andrew Leifer <leifer@fas.harvard.edu>
2009-10-12 15:16:22 -0400

    Implemented Structure for WormAnalysisParam

---
+++ MyLibs/WormAnalysis.c
@@ -0,0 +1,72 @@
+#include <stdio.h>
+
+//OpenCV Headers
+#include <cxcore.h>
+#include <highgui.h>
+#include <cv.h>
+
+
+
+
+
+/*
+ * Illuminate a segment of the worm. Either the right, or left side.
+ * Takes a sequence of the centerline. Uses the preceeding point to find a vector
+ * along the centerline, than draws a rectangle perpendicular to this vector, a radius rsquared pixels
+ * away from the centerline
+ */
+void IlluminateWormSegment(IplImage** image, CvSeq* centerline, CvSeq* Boundary, int segment){
+	int PRINTOUT=0;
+	if (segment <1) {
+		if (PRINTOUT) printf("ERROR: segment <1 :  Choose a segment along the worm that is at least 1.\n ");
+		return;
+	}
+
+	int rfactor=2;
+
+	CvPoint* PtAlongCenterline;
+	CvPoint* PrevPtAlongCenterline;
+	CvPoint* PtAlongBoundary;
+	CvPoint* PrevPtAlongBoundary;
+
+
+	CvPoint VecToBound; //Vector Perpendicular to the segment
+	CvPoint PrevVecToBound;
+
+	PtAlongCenterline=(CvPoint*) cvGetSeqElem(centerline,segment);
+	PrevPtAlongCenterline=(CvPoint*) cvGetSeqElem(centerline,segment-1);
+
+	PtAlongBoundary=(CvPoint*) cvGetSeqElem(Boundary,segment);
+	PrevPtAlongBoundary=(CvPoint*) cvGetSeqElem(Boundary,segment-1);
+
+	VecToBound= cvPoint(PtAlongBoundary->x - PtAlongCenterline->x ,PtAlongBoundary->y - PtAlongCenterline->y );
+	PrevVecToBound= cvPoint(PrevPtAlongBoundary->x - PrevPtAlongCenterline->x ,PrevPtAlongBoundary->y - PrevPtAlongCenterline->y );
+	if (PRINTOUT) printf("VecToBound=( %d,%d )\n",VecToBound.x, VecToBound.y);
+	if (PRINTOUT) printf("PrevVecToBound=( %d,%d )\n",PrevVecToBound.x, PrevVecToBound.y);
+
+	//What we want to do is double the length of the vectors and add them to the centerline
+	// To find a point that sticks out a specific radius from the worm.
+
+	CvPoint FarPt=cvPoint( PtAlongBoundary->x + 2* VecToBound.x ,  PtAlongBoundary->y + 2* VecToBound.y);
+	CvPoint PrevPt=cvPoint( PrevPtAlongBoundary->x + 2* PrevVecToBound.x ,  PrevPtAlongBoundary->y + 2* PrevVecToBound.y );
+
+
+
+	if (PRINTOUT) printf("Before cvFillConvexPoly\n");
+
+	//cvRectangle(*image,cvPoint(10,10),cvPoint(300,300),cvScalar(255,255,255),1,CV_AA,0);
+	//cvFillPoly(*image,pt,nRectpoints,nRectangles,cvScalar(255,255,255),CV_AA,0);
+	//{&FarPt, &PrevPT, PtAlongCenterline, PrevPtAlongCenterline};
+	// JUST
+	CvPoint myPolygon[4];
+	myPolygon[0]=FarPt;
+	myPolygon[1]=PrevPt;
+	myPolygon[3]=*PtAlongCenterline;
+	myPolygon[2]=*PrevPtAlongCenterline;
+	if (PRINTOUT) printf("FarPt=(%d,%d)\nPrevPt=(%d,%d)\n*PtAlongCenterline=(%d,%d)\n*PrevPtAlongCenterline=(%d,%d)\n",FarPt.x,FarPt.y,PrevPt.x,PrevPt.y,PtAlongBoundary->x,PtAlongBoundary->y,PrevPtAlongCenterline->x,PrevPtAlongCenterline->y);
+	cvFillConvexPoly(*image,myPolygon,4,cvScalar(255,255,255),CV_AA);
+	if (PRINTOUT) printf("After cvFillConvexPoly\n");
+
+
+
+}
---
+++ MyLibs/WormAnalysis.h
@@ -0,0 +1,41 @@
+/*
+ * WormAnalysis.h
+ *
+ *  Created on: Oct 12, 2009
+ *      Author: andy
+ *
+ *      This library contains functions that are specific to analyzing worms.
+ *
+ *      Functions in this library depend on:
+ *      	AndysOpenCVLib.h
+ *      	AndysComputations.h
+ */
+
+#ifndef WORMANALYSIS_H_
+#define WORMANALYSIS_H_
+
+typedef struct WormAnalysisParamStruct{
+	int LengthScale;
+	int LengthOffset;
+	int BinThresh;
+	int GaussSize;
+} WormAnalysisParam
+
+
+/*
+ * Illuminate a segment of the worm. Either the right, or left side.
+ * Takes a sequence of the centerline. Uses the preceeding point to find a vector
+ * along the centerline, than draws a rectangle perpendicular to this vector, a radius rsquared pixels
+ * away from the centerline
+ */
+void IlluminateWormSegment(IplImage** image, CvSeq* centerline, CvSeq* Boundary, int segment);
+
+
+
+
+
+
+
+
+
+#endif /* WORMANALYSIS_H_ */
