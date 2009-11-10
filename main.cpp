015b7eddf664b2a177b3237a1339f928aa43697a
unknown <Andy@.(none)>
2009-11-10 11:26:52 -0500

    Changed comment.

--- main.cpp
+++ main.cpp
@@ -92,7 +92,7 @@ int main (int argc, char** argv){
 	CalibData* Calib =CreateCalibData(cvSize(NSIZEX,NSIZEY),cvSize(NSIZEX,NSIZEY));
 	int ret=LoadCalibFromFile(Calib,"calib.dat");
 	if (ret!=0){
-		printf("Error reading in calibration!! \n");
+		printf("Error reading in calibrationfile!!\nPlease run CalibrateApparatus to generate calibration file calib.dat\nThank you.\nGoodbye.\n");
 		return 0;
 	}
 
