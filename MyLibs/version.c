c46baca43bea67053c9bf141f808da13fff0b736
Andrew Leifer <leifer@fas.harvard.edu>
2009-12-03 17:36:25 -0500

    blah

--- MyLibs/version.c
+++ MyLibs/version.c
@@ -1,3 +1,3 @@
 #include "version.h"
-const char * build_git_sha = "755e1a6ac2e835485cdeffa3d0a75846707e58f3";
-const char * build_git_time = "Thu Dec  3 17:06:19 EST 2009";
+const char * build_git_sha = "e6136262b821fdc118b544f8fdab97dee97ea60d";
+const char * build_git_time = "Thu Dec  3 17:07:39 EST 2009";
