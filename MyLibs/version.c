a486f521cd9c34046b7b3bb3ea3a2e466280708d
unknown <Andy@.(none)>
2009-12-03 11:57:10 -0500

    Recompiled

--- MyLibs/version.c
+++ MyLibs/version.c
@@ -1,3 +1,3 @@
 #include "version.h"
-const char * build_git_sha = "a9b9fc81db784d809ac0feaadd0dae1e8be58572";
-const char * build_git_time = "Thu Dec  3 11:53:16 EST 2009";
+const char * build_git_sha = "21a9d03c20f0f4a8f6ae44be4ce20273fce3bd1e";
+const char * build_git_time = "Thu Dec  3 11:56:56 EST 2009";
