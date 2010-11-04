72970a26ef5a0844140eeb60f1e43fe5223f7816
Andrew Leifer <leifer@fas.harvard.edu>
2010-11-04 13:38:40 -0400

    added MCD_Frame

---
+++ YMLUtils/MCD_FRAME.m
@@ -0,0 +1,23 @@
+classdef MCD_FRAME
+    %mind control data
+    properties
+        FrameNumber
+        TimeElapsed
+        Head
+        Tail
+        BoundaryA
+        BoundaryB
+        SegmentedCenterline
+        DLPisOn
+        FloodLightIsOn
+        IllumInvert
+        IllumFlipLR
+        IllumRectOrigin
+        IllumRectRadius
+        StageVelocity
+        ProtocolIsOn
+        ProtocolStep
+    end
+        
+end
+    
